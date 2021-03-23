722843a0ff168d8a26420ef2cac74e5ceffd050d
Brian Lopez <brianlopezmedina12@gmail.com>
2021-03-22 21:49:35 -0600

    alfin se subio bien

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBIFje5tUjgQJLmywaSAuPoUKmb9cD8PBs",
+    authDomain: "foodsmil.firebaseapp.com",
+    databaseURL: "https://foodsmil-default-rtdb.firebaseio.com",
+    projectId: "foodsmil",
+    storageBucket: "foodsmil.appspot.com",
+    messagingSenderId: "621939755656",
+    appId: "1:621939755656:web:14bf62c3054424b655ad3d",
+    measurementId: "G-JV1V68NML7"
+  };
+export default firebaseConfig;
